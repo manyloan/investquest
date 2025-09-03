1afa9590842776e8497e61e47fd13e8a871163e8
Haniel Loan <haniel.loan@gmail.com>
2025-09-03 11:15:57 -0300

    init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    // Isto é o equivalente ao <body> do exemplo
+    <div className="bg-slate-900 min-h-screen flex items-center justify-center">
+      {/* Isto é o equivalente ao <h1> do exemplo */}
+      <h1 className="text-4xl font-bold underline text-sky-400">
+        InvestQuest com Tailwind CSS!
+      </h1>
+    </div>
+  )
+}
+
+export default App
